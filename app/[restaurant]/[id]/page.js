1ed57e4ce47f9ea7f3db98f956e8d44a10858d2c
Michael R <m.romanov.biz@gmail.com>
2025-02-05 12:37:46 +0400

    cart edited

--- app/[restaurant]/[id]/page.js
+++ app/[restaurant]/[id]/page.js
@@ -7,10 +7,17 @@ export default function RestaurantPage() {
   return (
       <div className='w-full h-dvh flex relative pt-4'>
         <RestaurantMenu/>
-        <div className='rounded-lg w-full h-1/2 flex flex-col relative'>
+        <div className='rounded-lg overflow-hidden w-full h-72 flex flex-col relative'>
           <div style={{background: `url(/restaurants/slice-cut-from-classic-pepperoni-pizza-with-green-pepper-rolls.jpeg) center center/cover no-repeat`}} className='w-full h-full relative before:content-[""] before:w-full before:h-full before:top-0 before:left-0 before:absolute before:bg-black/65 before:-z-0 '>
-              <div className='z-0 relative p-4 text-accentText flex flex-col justify-between h-full'>
-                  <h1 className='text-5xl'>Кафе: РУБИН</h1>
+              <div className='z-0 relative p-4 text-accentText flex flex-col-reverse justify-between h-full'>
+                <div>
+                  <h1 className='text-5xl mb-3'>Кафе: РУБИН</h1>
+                  <div className='w-1/6'>
+                    <h4>Средний чек</h4>
+                    <div className='h-px w-full bg-menuItems'></div>
+                    <h4>500-700р</h4>
+                  </div>
+                </div>
               </div>
           </div>
         </div>
--- app/components/RestaurantCart/RestaurantCart.js
+++ app/components/RestaurantCart/RestaurantCart.js
@@ -1,28 +1,37 @@
 export default function RestaurantCart(){
     return(
-        <div className='w-1/2 p-4 bg-white mx-5 rounded-lg'>
-            <h3 className='text-xl font-semibold'>Корзина</h3>
-            <div className='w-full grid grid-cols-2 text-center my-5 rounded-lg p-1 bg-lightMuted'>
-                <h4 className='py-3.5 bg-primary text-menuItems rounded-lg'>Доставка</h4>
-                <h4 className='py-3.5 text-secondaryText'>На вынос</h4>
+        <div className='w-1/2 h-dvh p-4 bg-white mx-5 rounded-lg flex flex-col justify-between'>
+            <div>
+                <h3 className='text-xl font-semibold'>Корзина</h3>
+                <div className='w-full grid grid-cols-2 text-center my-5 rounded-lg p-1 bg-lightMuted'>
+                    <h4 className='cursor-pointer py-3.5 bg-primary text-menuItems rounded-lg'>Доставка</h4>
+                    <h4 className='cursor-pointer py-3.5 text-secondaryText'>На вынос</h4>
+                </div>
+                <ul>
+                    <li className='w-full h-max flex items-center justify-between'>
+                        <div className='grid grid-cols-3 gap-4'>
+                            <div className='aspect-square rounded-md w-full bg-black'
+                            style={{background: `url(/products/pizza/pizza1.png) center center/cover no-repeat`}}
+                            ></div>
+                            <div className='col-span-2'>
+                                <h4>Пицца деревенская</h4>
+                                <h5 className='text-muted'>550г</h5>
+                                <h5>450р</h5>
+                            </div>
+                        </div>
+                        <div className='flex items-center bg-globalBackground h-max rounded-lg'>
+                            <div className='py-1 px-3 cursor-pointer'>-</div>
+                            <div className='py-1'>2</div>
+                            <div className='py-1 px-3 cursor-pointer'>+</div>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <ul>
-                <li className='w-full flex justify-between'>
-                    <div className='aspect-square rounded-md w-1/4 bg-black'
-                    style={{background: `url(/products/pizza/pizza1.png) center center/cover no-repeat`}}
-                    ></div>
-                    <div>
-                        <h4>Пицца деревенская</h4>
-                        <h5 className='text-muted'>550г</h5>
-                        <h5>450р</h5>
-                    </div>
-                    <div className='flex items-center bg-globalBackground h-max translate-y-1/2 rounded-lg'>
-                        <div className='py-1.5 px-3 cursor-pointer'>-</div>
-                        <div className='py-1.5'>2</div>
-                        <div className='py-1.5 px-3 cursor-pointer'>+</div>
-                    </div>
-                </li>
-            </ul>
+            <div>
+                <div className='w-full border border-primary-200 border-dashed'></div>
+                <div className='text-muted font-medium text-lg py-3'>Доставка - <span className='text-green-800'>бесплатно</span></div>
+                <div className='button cart flex justify-between'>Оформить <span>450р</span></div>
+            </div>
         </div>
     )
 }
